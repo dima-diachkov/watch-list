c68118e05af228ee9e134c57a14b71614e296a4b
dima-diachkov <dima-diachkov>
2022-10-25 00:32:11 +0300

    add menu to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,75 @@
-import { Box, Grid, Container } from '@mui/material';
+import { Box, Grid, Container, Link } from '@mui/material';
 import React from 'react';
 
 export const Footer = () => {
     return (
         <footer>
-            <Box>
+            <Box
+                px={{ sm: 4 }}
+                py={{ sm: 4 }}
+                bgcolor='primary.light'
+                color='white'
+            >
                 <Container maxWidth='lg'>
                     <Grid container spacing={5}>
                         <Grid item xs={12} sm={4}>
                             <Box borderBottom={1}>
                                 HELP
+                            </Box>
+                            <Box>
+                                <Link href='/' color='inherit'>
+                                    CONTACT
+                                </Link>
+                            </Box>
+                            <Box>
+                                <Link href='/' color='inherit'>
+                                    CONTACT
+                                </Link>
+                            </Box>
+                            <Box>
+                                <Link href='/' color='inherit'>
+                                    CONTACT
+                                </Link>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Box borderBottom={1}>
+                                HELP
+                            </Box>
+                            <Box>
+                                <Link href='/' color='inherit'>
+                                    CONTACT
+                                </Link>
+                            </Box>
+                            <Box>
+                                <Link href='/' color='inherit'>
+                                    CONTACT
+                                </Link>
+                            </Box>
+                            <Box>
+                                <Link href='/' color='inherit'>
+                                    CONTACT
+                                </Link>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Box borderBottom={1}>
+                                HELP
+                            </Box>
+                            <Box>
+                                <Link href='/' color='inherit'>
+                                    CONTACT
+                                </Link>
+                            </Box>
+                            <Box>
+                                <Link href='/' color='inherit'>
+                                    CONTACT
+                                </Link>
+                            </Box>
+                            <Box>
+                                <Link href='/' color='inherit'>
+                                    CONTACT
+                                </Link>
                             </Box>
                         </Grid>
                     </Grid>
