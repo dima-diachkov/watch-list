60ecdefa62b1c0f7b3521c9a7cb2f509e8205caf
dima-diachkov <dima-diachkov>
2022-10-25 20:40:19 +0300

    add typography

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,74 +1,98 @@
-import { Box, Grid, Container, Link } from '@mui/material';
+import { Box, Grid, Container, Link, Typography } from '@mui/material';
 import React from 'react';
 
 export const Footer = () => {
     return (
         <footer>
             <Box
-                px={{ sm: 4 }}
-                py={{ sm: 4 }}
+                px={{ sm: 3 }}
+                py={{ sm: 3 }}
                 bgcolor='primary.light'
                 color='white'
             >
                 <Container maxWidth='lg'>
-                    <Grid container spacing={5}>
+                    <Grid container spacing={2} sx={{px: 4}}>
                         <Grid item xs={12} sm={4}>
                             <Box borderBottom={1}>
-                                HELP
+                                <Typography variant='h6'>
+                                    HELP
+                                </Typography>
                             </Box>
                             <Box>
-                                <Link href='/' color='inherit'>
-                                    CONTACT
+                                <Link href='/' color='inherit' underline='none'>
+                                    <Typography>
+                                        Contact
+                                    </Typography>
                                 </Link>
                             </Box>
                             <Box>
-                                <Link href='/' color='inherit'>
-                                    CONTACT
+                                <Link href='/' color='inherit' underline='none'>
+                                    <Typography>
+                                        Contact
+                                    </Typography>
                                 </Link>
                             </Box>
                             <Box>
-                                <Link href='/' color='inherit'>
-                                    CONTACT
+                                <Link href='/' color='inherit' underline='none'>
+                                    <Typography>
+                                        Contact
+                                    </Typography>
                                 </Link>
                             </Box>
                         </Grid>
                         <Grid item xs={12} sm={4}>
                             <Box borderBottom={1}>
-                                HELP
+                                <Typography variant='h6'>
+                                    HELP
+                                </Typography>
                             </Box>
                             <Box>
-                                <Link href='/' color='inherit'>
-                                    CONTACT
+                                <Link href='/' color='inherit' underline='none'>
+                                    <Typography>
+                                        Contact
+                                    </Typography>
                                 </Link>
                             </Box>
                             <Box>
-                                <Link href='/' color='inherit'>
-                                    CONTACT
+                                <Link href='/' color='inherit' underline='none'>
+                                    <Typography>
+                                        Contact
+                                    </Typography>
                                 </Link>
                             </Box>
                             <Box>
-                                <Link href='/' color='inherit'>
-                                    CONTACT
+                                <Link href='/' color='inherit' underline='none'>
+                                    <Typography>
+                                        Contact
+                                    </Typography>
                                 </Link>
                             </Box>
                         </Grid>
                         <Grid item xs={12} sm={4}>
                             <Box borderBottom={1}>
-                                HELP
+                                <Typography variant='h6'>
+                                    HELP
+                                </Typography>
                             </Box>
                             <Box>
-                                <Link href='/' color='inherit'>
-                                    CONTACT
+                                <Link href='/' color='inherit' underline='none'>
+                                    <Typography>
+                                        Contact
+                                    </Typography>
                                 </Link>
                             </Box>
                             <Box>
-                                <Link href='/' color='inherit'>
-                                    CONTACT
+                                <Link href='/' color='inherit' underline='none'>
+                                    <Typography>
+                                        Contact
+                                    </Typography>
                                 </Link>
                             </Box>
                             <Box>
-                                <Link href='/' color='inherit'>
-                                    CONTACT
+                                <Link href='/' color='inherit' underline='none'>
+                                    <Typography>
+                                        Contact
+                                    </Typography>
                                 </Link>
                             </Box>
                         </Grid>
