488bde3366d59d785804ceaac226608629a9ccca
dima diachkov <diachkov.dima@gmail.com>
2023-01-04 23:27:42 +0200

    styled header container

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -13,7 +13,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 export const NavBar = () => {
   return (
     <AppBar position="fixed">
-      <Container fixed>
+      <Container fixed sx={{"&.MuiContainer-maxWidthLg": { px: 0}}}>
         <Toolbar>
           <IconButton edge="start" color="inherit" aria-label="menu">
             <MenuIcon />
