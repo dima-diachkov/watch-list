17d575fd30ebb0b3f45feca7a8184d45d4753c39
dima diachkov <diachkov.dima@gmail.com>
2023-03-02 12:05:01 +0200

    add item prop to grid

--- src/components/Trailers/GridCard/GridCard.tsx
+++ src/components/Trailers/GridCard/GridCard.tsx
@@ -10,7 +10,7 @@ export const GridCard = () => (
   <Container sx={{ maxWidth: 1200, flexGrow: 1 }}>
     <Grid container>
       {trailersData.map((card) => (
-        <Grid container justifyContent="space-around" key={card['id']} xs={12} sm={6} md={4} lg={3}>
+        <Grid item container justifyContent="space-around" key={card['id']} xs={12} sm={6} md={4} lg={3}>
           <Grid item>
             <VideoCard
               filmName={card['original_title']}
